df4c9c20992ade0e555df2441907d84369a0119d
jasaga2401 <jasaga@protonmail.com>
2023-12-20 14:39:29 +0000

    Add project files.

---
+++ slider/slider/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace slider
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            rotatingLabel.Rotation = value;
+            sliderValue.Text = value.ToString();
+        }
+    }
+}
